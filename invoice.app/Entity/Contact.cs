79c2aecc25fd133234422f6a2c64f1afa9f245e5
Jam0l <xjamoliddin27@gmail.com>
2022-01-28 19:21:37 +0500

    Partner -> Contact

--- invoice.app/Entity/Contact.cs
+++ invoice.app/Entity/Contact.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace invoice.app.Entity;
 
-public class Partner
+public class Contact
 {
     public Guid Id { get; set; }
     
---
+++ invoice.app/Migrations/20220128141613_contact.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace invoice.app.Migrations
+{
+    public partial class contact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
